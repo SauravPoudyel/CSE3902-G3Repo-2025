fd727854ea87a7e59d6777afdcee9adabd43f27c
PaytonYaffe <payton@rmygroup.com>
2025-02-08 09:52:58 -0500

    Added mob cycle
    havent tested yet

--- Sprint0/Game/Entities/Mob.cs
+++ Sprint0/Game/Entities/Mob.cs
@@ -8,10 +8,16 @@ namespace Sprint0
 {
     public class Mob : Entity
     {
+        enum MobType {
+            BossTank,
+            SmallEnemy,
+
+        }
         private static int projectileCounter = 0;
         private ContentManager content;
         private float timer;
         private ISprite spriteSecondary;
+        private MobType mobType;
 
         public Mob(ContentManager content) 
         {
@@ -22,11 +28,36 @@ namespace Sprint0
             AddSprite("BossTankBase", new AnimatedSprite(0.3f));
             sprites["BossTankBase"].LoadContent(content, "TDTanksAllSprites", 641, 661, 123, 144, 1);
 
+            AddSprite("SmallEnemyTank", new AnimatedSprite(0.3f));
+            sprites["SmallEnemyTank"].LoadContent(content, "TDTanksAllSprites", 768, 256, 95, 113, 1);
+
             AddSprite("Cannon", new AnimatedSprite(0.3f));
             sprites["Cannon"].LoadContent(content, "TDTanksAllSprites", 832, 186, 28, 64, 1);  
 
             SetSprite(sprites["BossTankBase"]);
             SetSpriteSecondary(sprites["Cannon"]);
+        }
+        private void SetItemType(MobType mobType) {
+            this.mobType = mobType;
+            this.SetSprite(this.mobType.ToString());
+        }
+
+        public string GetItemType() {
+            return mobType.ToString();
+        }
+        public void CycleItemNext() {
+            if(mobType == MobType.BossTank) {
+                SetItemType(MobType.SmallEnemy);
+            } else if (mobType == MobType.SmallEnemy) {
+                SetItemType(MobType.BossTank);
+            }        
+        }
+        public void CycleItemPrev() {
+            if(mobType == MobType.BossTank) {
+                SetItemType(MobType.SmallEnemy);
+            } else if (mobType == MobType.BossTank) {
+                SetItemType(MobType.SmallEnemy);
+            }            
         }
         public override void Update(GameTime gameTime)
         {
