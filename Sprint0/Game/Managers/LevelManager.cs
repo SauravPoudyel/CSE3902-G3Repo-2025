90ac5e861f83541acf02d373c1e601f388558fc4
manzlerh <manzlerh@gmail.com>
2025-02-28 11:43:15 -0500

    Moved LevelParseTest1 to Data
    Small organization change: simply moved the LevelParseTest1.csv file from Content to Data.

--- Sprint0/Game/Managers/LevelManager.cs
+++ Sprint0/Game/Managers/LevelManager.cs
@@ -13,20 +13,8 @@ public class LevelManager
         level = new Level();
     }
     public void LoadContent(ContentManager content) {
-        // Commenting out manual level loading while testing Level parser
-        /* level.AddPlayer(content, new Vector2(100, 100));
-        level.AddBlock(content, new Vector2(300, 300));
-        level.AddEnemy(content, MobType.BossTank, new Vector2(500, 500));
-        level.AddItem(content, new Vector2(700, 700)); */
-
-        // string filePath = Path.Combine("Sprint0", "Data", "LevelParseTest1.csv");
         string projectDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
-        string filePath2 = Path.Combine(projectDirectory, "Content\\LevelParseTest1.csv");
-        // string filePath = Path.Combine(AppDomain.CurrentDomain.RelativeSearchPath, "Data", "LevelParseTest1.csv");
-        
-        // string filePath = content.RootDirectory + "\\LevelParseTest1.csv";
-        // C:\Users\manzl\Documents\GitHub\CSE3902-G3Repo-2025\Sprint0\Content\LevelParseTest1.csv
-        // string filePath = "GitHub\\CSE3902-G3Repo-2025\\Sprint0\\Content\\LevelParseTest1.csv";
+        string filePath2 = Path.Combine(projectDirectory, "Data\\LevelParseTest1.csv");
         level = CSVParser.ParseLevel(filePath2, content);
     }
     public Dictionary<string, Entity> LoadLevelEntities() {
