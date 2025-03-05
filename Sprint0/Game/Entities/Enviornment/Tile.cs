d7350cd61e28615e0dd054d828a22dc4fe931db9
manzlerh <manzlerh@gmail.com>
2025-03-05 12:21:17 -0500

    New Tile Types, CSVLevelParser refactoring
    - Added many new tile types: Snow, Ice, Roads...
    - Refactored tileType switch statement in CSVLevelParser to use Enum.TryParse. NOTE: this means for the CSV levels each entry should use the proper, full name for a tile enum. NOT the previous abbreviations of G for grass, S for sand etc.
    - Added LevelTilesParseTest3.csv to test new tile types

--- Sprint0/Game/Entities/Enviornment/Tile.cs
+++ Sprint0/Game/Entities/Enviornment/Tile.cs
@@ -12,8 +12,18 @@ namespace Sprint0
             Sand,
             Concrete,
             Dirt,
+            Snow,
+            Ice,
+            RightGrassLeftSand,
             LeftGrassRightSand,
+            TopGrassBottomSand,
+            BottomGrassTopSand,
+            GrassCrossroad,
+            SandCrossroad,
+            VerticalGrassRoad,
+            VerticalSandRoad,
             LeftGrassRightSandRoad,
+            RightGrassLeftSandRoad,
             HorizontalGrassRoad,
             HorizontalSandRoad
         }
@@ -42,10 +52,16 @@ namespace Sprint0
                     sprite.LoadContent(content, "TDTanksAllSprites", 256, 0, 128, 128, 1);
                     break;
                 case TileType.Concrete:
-                    sprite.LoadContent(content, "TDTanksAllSprites", 256, 256, 128, 128, 1);
+                    sprite.LoadContent(content, "TDTanksAllSprites", 640, 1256, 128, 128, 1);
                     break;
                 case TileType.Dirt:
-                    sprite.LoadContent(content, "TDTanksAllSprites", 384, 384, 128, 128, 1);
+                    sprite.LoadContent(content, "TDTanksAllSprites", 640, 1128, 128, 128, 1);
+                    break;
+                case TileType.Snow:
+                    sprite.LoadContent(content, "TDTanksAllSprites", 0, 1384, 128, 128, 1);
+                    break;
+                case TileType.Ice:
+                    sprite.LoadContent(content, "TDTanksAllSprites", 512, 1768, 128, 128, 1);
                     break;
                 case TileType.LeftGrassRightSand:
                     sprite.LoadContent(content, "TDTanksAllSprites", 640, 128, 128, 128, 1);
@@ -53,11 +69,35 @@ namespace Sprint0
                 case TileType.LeftGrassRightSandRoad:
                     sprite.LoadContent(content, "TDTanksAllSprites", 512, 640, 128, 128, 1);
                     break;
+                case TileType.RightGrassLeftSandRoad:
+                    sprite.LoadContent(content, "TDTanksAllSprites", 0, 2 * 128, 128, 128, 1);
+                    break;
                 case TileType.HorizontalGrassRoad:
                     sprite.LoadContent(content, "TDTanksAllSprites", 0, 768, 128, 128, 1);
                     break;
                 case TileType.HorizontalSandRoad:
                     sprite.LoadContent(content, "TDTanksAllSprites", 256, 896, 128, 128, 1);
+                    break;
+                case TileType.RightGrassLeftSand:
+                    sprite.LoadContent(content, "TDTanksAllSprites", 4 * 128, 0 * 128, 128, 128, 1);
+                    break;
+                case TileType.TopGrassBottomSand:
+                    sprite.LoadContent(content, "TDTanksAllSprites", 4 * 128, 1 * 128, 128, 128, 1);
+                    break;
+                case TileType.BottomGrassTopSand:
+                    sprite.LoadContent(content, "TDTanksAllSprites", 4 * 128, 2 * 128, 128, 128, 1);
+                    break;
+                case TileType.GrassCrossroad:
+                    sprite.LoadContent(content, "TDTanksAllSprites", 1 * 128, 5 * 128, 128, 128, 1);
+                    break;
+                case TileType.SandCrossroad:
+                    sprite.LoadContent(content, "TDTanksAllSprites", 3 * 128, 1 * 128, 128, 128, 1);
+                    break;
+                case TileType.VerticalGrassRoad:
+                    sprite.LoadContent(content, "TDTanksAllSprites", 1 * 128, 7 * 128, 128, 128, 1);
+                    break;
+                case TileType.VerticalSandRoad:
+                    sprite.LoadContent(content, "TDTanksAllSprites", 2 * 128, 6 * 128, 128, 128, 1);
                     break;
             }
         }
