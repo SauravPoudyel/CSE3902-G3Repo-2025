9835b2a19d9bf59d9babdc0aa779865e4d431322
Leo Chen <chen.11937@osu.edu>
2025-03-31 21:55:50 -0400

    fix missing paramters

--- Sprint0/Game/UI/Inventory/PlayerInventory.cs
+++ Sprint0/Game/UI/Inventory/PlayerInventory.cs
@@ -26,7 +26,7 @@ namespace Sprint0
             hudElements.Add(new XPHUD(game.GraphicsDevice));
             hudElements.Add(new KillCountHUD(game.GameManager, content.Load<Texture2D>("UI/KillCountHUDIcon"), content.Load<Texture2D>("2DTanksSprites")));
 
-           
+            paramters = new Dictionary<string, object>
             {
                 { "gameManager", game.GameManager },
                 { "game", game}
