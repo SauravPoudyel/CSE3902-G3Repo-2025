460e51d92405c0cd0b9617ba0476448e55a51805
PaytonYaffe <payton@rmygroup.com>
2025-04-04 13:09:42 -0400

    Stats no longer lags the game as bad

--- Sprint0/Game/UI/StatsScreen.cs
+++ Sprint0/Game/UI/StatsScreen.cs
@@ -8,9 +8,25 @@ using static Sprint0.EntityKeys;
 
 namespace Sprint0
 {
+    public class StatsItem
+    {
+        public string Name { get; set; }
+        public Texture2D Icon { get; set; }
+        public Rectangle IconRect { get; set; }
+        public int Amount {get ; set;}
+
+        public StatsItem(string name, Texture2D icon, Rectangle rectangle, int amount)
+        {
+            Name = name;
+            Icon = icon;
+            IconRect = rectangle;
+            Amount = amount;
+        }
+    }
     public class StatsScreen : IScreen
     {
         private Dictionary<string, Rectangle> mobList = new Dictionary<string, Rectangle>();
+        private List<StatsItem> statsItems;
         private Rectangle windowRectangle;
         private Texture2D backgroundTexture;
         private Texture2D spriteSheet1;
@@ -21,6 +37,7 @@ namespace Sprint0
         {
             ContentManager content = game.Content;
             GraphicsDevice graphicsDevice = game.GraphicsDevice;
+            statsItems = new List<StatsItem>();
             //Set size of the window
             int width = Globals.SCREENWIDTH/2;
             int height = Globals.SCREENHEIGHT/2;
@@ -36,17 +53,17 @@ namespace Sprint0
             //Add Text and Images of rectangles
             spriteSheet1 = content.Load<Texture2D>("TDTanksAllSprites");
             spriteSheet2 = content.Load<Texture2D>("TDTowerDefenseSprites");
-            mobList.Add("BossTank", new Rectangle(641, 661, 123, 144));
-            mobList.Add("HealerTank", new Rectangle(1126, 334, 76, 72));
-            mobList.Add("HoveringTank", new Rectangle(1135, 180, 86, 92));
-            mobList.Add("Plane", new Rectangle(2183, 1411, 135, 134));
-            mobList.Add("ShieldTank", new Rectangle(768, 0, 94, 97));
-            mobList.Add("ShipVertical", new Rectangle(1135, 840, 68, 116));
-            mobList.Add("ShipHorizontal", new Rectangle(1135, 840, 68, 116));
-            mobList.Add("SmallEnemy", new Rectangle(768, 256, 95, 113));
-            mobList.Add("StealthTank", new Rectangle( 876, 783, 84, 80));
-            mobList.Add("SwarmingTank", new Rectangle( 1126, 275, 53, 56));
-            mobList.Add("Turret", new Rectangle(2444, 908, 104, 104));
+            statsItems.Add(new StatsItem("BossTank",spriteSheet1, new Rectangle(641, 661, 123, 144), Globals.PlayerData.GetInt("BossTankKilled")));
+            statsItems.Add(new StatsItem("HealerTank", spriteSheet1, new Rectangle(1126, 334, 76, 72), Globals.PlayerData.GetInt("HealerTankKilled")));
+            statsItems.Add(new StatsItem("HoveringTank", spriteSheet1, new Rectangle(1135, 180, 86, 92), Globals.PlayerData.GetInt("HoveringTankKilled")));
+            statsItems.Add(new StatsItem("Plane", spriteSheet2, new Rectangle(2183, 1411, 135, 134), Globals.PlayerData.GetInt("PlaneKilled")));
+            statsItems.Add(new StatsItem("ShieldTank", spriteSheet1, new Rectangle(768, 0, 94, 97), Globals.PlayerData.GetInt("ShieldTankKilled")));
+            statsItems.Add(new StatsItem("ShipVertical", spriteSheet1, new Rectangle(1135, 840, 68, 116), Globals.PlayerData.GetInt("ShipKilled")));
+            statsItems.Add(new StatsItem("ShipHorizontal", spriteSheet1, new Rectangle(1135, 840, 68, 116), Globals.PlayerData.GetInt("ShipKilled")));
+            statsItems.Add(new StatsItem("SmallEnemy", spriteSheet1, new Rectangle(768, 256, 95, 113), Globals.PlayerData.GetInt("SmallEnemyKilled")));
+            statsItems.Add(new StatsItem("StealthTank", spriteSheet1, new Rectangle( 876, 783, 84, 80), Globals.PlayerData.GetInt("StealthTankKilled")));
+            statsItems.Add(new StatsItem("SwarmingTank", spriteSheet1, new Rectangle( 1126, 275, 53, 56), Globals.PlayerData.GetInt("SwarmingTankKilled")));
+            statsItems.Add(new StatsItem("Turret", spriteSheet2, new Rectangle(2444, 908, 104, 104), Globals.PlayerData.GetInt("TurretKilled")));
 
             //Add close screen buttons
 
@@ -65,44 +82,37 @@ namespace Sprint0
             
             spriteBatch.Draw(backgroundTexture, windowRectangle, Color.White);
             //loop for each mob to draw
-            int i = 0;
             int secondColumn = 0;
-            foreach (MobType mob in EntityKeys.MobType.GetValues(typeof (MobType)))
+            Vector2 iconPos;
+            Rectangle iconDestinationRect;
+            Vector2 textPos;
+            string itemText;
+            Vector2 textSize;
+            Rectangle textRect;
+            for (int i = 0; i < 11; i++)
             {
-                Vector2 iconPos;
-                Rectangle iconDestinationRect;
-                Vector2 textPos;
-                string itemText;
-                Vector2 textSize;
-                Rectangle textRect;
+                StatsItem item = statsItems[i];
                 if (i > 5)
                 {
-                    iconPos = new Vector2(windowRectangle.X + 20, windowRectangle.Y + 50 + i * 60);
-                    iconDestinationRect = new Rectangle((int)iconPos.X + 500, (int)iconPos.Y - 350, 40, 40);
-                    textPos = new Vector2(iconPos.X + 50 + 500, iconPos.Y + 10 + 300);
-                    itemText = mob + " Killed " + Globals.PlayerData.GetInt(mob.ToString() + "killed");
+                    iconPos = new Vector2(windowRectangle.X + 20 + 500, windowRectangle.Y + 50 + i * 60 - 350);
+                    iconDestinationRect = new Rectangle((int)iconPos.X, (int)iconPos.Y, 40, 40);
+                    textPos = new Vector2(iconPos.X + 50, iconPos.Y + 10);
+                    itemText = item.Name + " Killed " + Globals.PlayerData.GetInt(item.Name.ToString() + "killed");
                     textSize = font.MeasureString(itemText);
                     textRect = new Rectangle((int)textPos.X + 500, (int)textPos.Y, (int)textSize.X, (int)textSize.Y);
+                    spriteBatch.Draw(item.Icon, iconDestinationRect, item.IconRect, Color.White);
+                    spriteBatch.DrawString(font, itemText, textPos, Color.White);
                 } else 
                 {
                     iconPos = new Vector2(windowRectangle.X + 20, windowRectangle.Y + 50 + i * 60);
                     iconDestinationRect = new Rectangle((int)iconPos.X, (int)iconPos.Y, 40, 40);
                     textPos = new Vector2(iconPos.X + 50 + secondColumn, iconPos.Y + 10);
-                    itemText = mob + " Killed " + Globals.PlayerData.GetInt(mob.ToString() + "killed");
+                    itemText = item.Name + " Killed " + Globals.PlayerData.GetInt(item.Name.ToString() + "killed");
                     textSize = font.MeasureString(itemText);
                     textRect = new Rectangle((int)textPos.X, (int)textPos.Y, (int)textSize.X, (int)textSize.Y);
+                    spriteBatch.Draw(item.Icon, iconDestinationRect, item.IconRect, Color.White);
+                    spriteBatch.DrawString(font, itemText, textPos, Color.White);
                 }
-                
-                if ((mob == MobType.Plane) || (mob == MobType.Turret)) 
-                {
-                    spriteBatch.Draw(spriteSheet2, iconDestinationRect, mobList[mob.ToString()], Color.White);
-                } else 
-                {
-                    spriteBatch.Draw(spriteSheet1, iconDestinationRect, mobList[mob.ToString()], Color.White);
-                }
-                spriteBatch.DrawString(font, itemText, textPos, Color.White);
-
-                i++;
             }
         }
     }
