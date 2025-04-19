a15fc3d27e9032daaed210e0a7abfcf57889492d
manzlerh <manzlerh@gmail.com>
2025-04-19 14:17:29 -0400

    Difficulty in Pause Menu, MobData added into Globals

--- Sprint0/Game/Globals.cs
+++ Sprint0/Game/Globals.cs
@@ -15,7 +15,7 @@ namespace Sprint0
         public static float FRAMETIME = 1f / 60f;
         public const float PLAYERFRAMETIME = 1f / 60f; // just so the slow powerup works
         private static PlayerData playerData;
-        private static MobData mobData;
+        public static MobData GlobalMobData = new MobData();
         public static SpriteFont FONT;
         public static string projectDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
         public static Random random = new Random();
